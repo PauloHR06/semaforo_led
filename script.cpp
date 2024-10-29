26c664a2b8541727a6f737b89a0a48bf489eba2a
PauloHR06 <paulo.ribeiro@sou.inteli.edu.br>
2024-10-29 14:55:58 -0300

    commit

--- script.cpp
+++ script.cpp
@@ -1,20 +1,18 @@
 void setup() {
-
+  // Configura os pinos 8, 9 e 10 como saídas para controlar os LEDs
   for (int i = 8; i <= 10; i++) {
     pinMode(i, OUTPUT);
   }
-
 }
 
 void loop() {
-
-  int ledPins[] = {8, 9, 10};
-
-  int *currentPin = ledPins;
+  int ledPins[] = {8, 9, 10}; // Array com os pinos dos LEDs
+  int *currentPin = ledPins;  // Ponteiro para o primeiro elemento do array
 
   for (int i = 0; i < 3; i++) {
-    digitalWrite(*(currentPin + i), HIGH); 
-    delay((i == 1) ? 6000 : 2000);         
-    digitalWrite(*(currentPin + i), LOW); 
+    digitalWrite(*(currentPin + i), HIGH);           // Acende o LED correspondente
+    delay((i == 1) ? 6000 : 2000);                   // Mantém o LED aceso (6 segundos para o pino 9, 2 segundos para os outros)
+    digitalWrite(*(currentPin + i), LOW);            // Apaga o LED
   }
 }
+
